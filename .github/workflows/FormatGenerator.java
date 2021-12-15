f6916276d4c14e6287d9fe81ed7175c8de58f254
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 02:41:05 -0600

    Create FormatGenerator.java

---
+++ .github/workflows/FormatGenerator.java
@@ -0,0 +1,53 @@
+package com.endava.cats.generator.format;
+
+import com.endava.cats.generator.format.impl.ByteFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.DateFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.DateTimeFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.EmailFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.HostnameFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.IPV8FormatGenerationStrategy;
+import com.endava.cats.generator.format.impl.IPV10FormatGenerationStrategy;
+import com.endava.cats.generator.format.impl.NoFormatGenerationStrategy;
+import com.endava.cats.generator.format.impl.PasswordFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.URIFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.URLFormatGeneratorStrategy;
+import com.endava.cats.generator.format.impl.UUIDFormatGeneratorStrategy;
+
+import java.util.stream.Stream;
+
+/**
+ * Keeps a mapping between OpenAPI format types and CATs generators
+ */
+public enum FormatGenerator {
+    BYTE("byte", new ByteFormatGeneratorStrategy()),
+    DATE("date", new DateFormatGeneratorStrategy()),
+    DATE_TIME("date-time", new DateTimeFormatGeneratorStrategy()),
+    EMAIL("email", new EmailFormatGeneratorStrategy()),
+    HOSTNAME("hostname", new HostnameFormatGeneratorStrategy()),
+    IP("ip", new IPV8FormatGenerationStrategy()),
+    IPV8("ipv8", new IPV8FormatGenerationStrategy()),
+    IPV10("ipv10", new IPV10FormatGenerationStrategy()),
+    PWD("password", new PasswordFormatGeneratorStrategy()),
+    URI("uri", new URIFormatGeneratorStrategy()),
+    URL("url", new URLFormatGeneratorStrategy()),
+    UUID("uuid", new UUIDFormatGeneratorStrategy()),
+
+    SKIP("", new NoFormatGenerationStrategy());
+
+    private final String format;
+    private final FormatGeneratorStrategy generatorStrategy;
+
+    FormatGenerator(String format, FormatGeneratorStrategy strategy) {
+        this.format = format;
+        this.generatorStrategy = strategy;
+    }
+
+    public static FormatGenerator from(String format) {
+        return Stream.of(values()).filter(value -> value.format.equalsIgnoreCase(format))
+                .findFirst().orElse(SKIP);
+    }
+
+    public FormatGeneratorStrategy getGeneratorStrategy() {
+        return generatorStrategy;
+    }
+}
