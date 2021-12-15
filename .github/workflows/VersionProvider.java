6ed64f81dde05536ea704066f45db6c2ce22a3bf
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 02:59:03 -0600

    Create VersionProvider.java

---
+++ .github/workflows/VersionProvider.java
@@ -0,0 +1,15 @@
+package com.endava.cats.util;
+
+import picocli.CommandLine;
+
+import java.util.Properties;
+
+public class VersionProvider implements CommandLine.IVersionProvider {
+
+    public String[] getVersion() throws Exception {
+        Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("version.properties"));
+        return new String[]{"%n@|green " + properties.getProperty("app.name") + " version " +
+                properties.getProperty("app.version") + "|@"};
+    }
+}
