9e766be8c9d03c14d14fe4be14ba453ae5dd093a
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 04:01:10 -0600

    Create ServiceData.java

---
+++ .github/workflows/ServiceData.java
@@ -0,0 +1,35 @@
+package com.endava.cats.io;
+
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.model.CatsHeader;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
+@Getter
+public class ServiceData {
+    private final String relativePath;
+    private final Collection<CatsHeader> headers;
+    private final String payload;
+    private final HttpMethod httpMethod;
+    @Builder.Default
+    private final boolean replaceRefData = true;
+    @Builder.Default
+    private final boolean addUserHeaders = true;
+    @Builder.Default
+    private final Set<String> skippedHeaders = new HashSet<>();
+    @Singular
+    private final Set<String> fuzzedFields;
+    @Singular
+    private final Set<String> fuzzedHeaders;
+    @Builder.Default
+    private final Set<String> pathParams = new HashSet<>();
+    @Builder.Default
+    private final Set<String> queryParams = new HashSet<>();
+
+}
