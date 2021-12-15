81bd0733d0e3de10028346a6ae0c82132253885f
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 03:44:36 -0600

    Create CatsTestReport.java

---
+++ .github/workflows/CatsTestReport.java
@@ -0,0 +1,20 @@
+package com.endava.cats.model.report;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class CatsTestReport {
+    private final List<CatsTestCaseSummary> summaryList;
+    private final int totalTests;
+    private final int success;
+    private final int warnings;
+    private final int errors;
+    private final long executionTime;
+    private final String timestamp;
+    private final String catsVersion;
+
+}
