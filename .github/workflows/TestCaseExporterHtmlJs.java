61d5ddaee070aac44423d0494b24331cba6d5816
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 03:57:13 -0600

    Create TestCaseExporterHtmlJs.java

---
+++ .github/workflows/TestCaseExporterHtmlJs.java
@@ -0,0 +1,30 @@
+package com.endava.cats.io;
+
+import com.endava.cats.model.report.CatsTestCaseSummary;
+import com.endava.cats.model.report.CatsTestReport;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ApplicationScoped
+@Named("htmlJs")
+public class TestCaseExporterHtmlJs extends TestCaseExporter {
+
+    @Override
+    public String[] getSpecificHelperFiles() {
+        return new String[]{"cats.png", "styles.css", "script.js", "jquery.min.js"};
+    }
+
+    @Override
+    public Map<String, Object> getSpecificContext(CatsTestReport report) {
+        Map<String, Object> context = new HashMap<>();
+        List<String> fuzzers = report.getSummaryList().stream().map(CatsTestCaseSummary::getFuzzer).distinct().collect(Collectors.toList());
+        context.put("FUZZERS", fuzzers);
+        context.put("JS", true);
+        return context;
+    }
+}
