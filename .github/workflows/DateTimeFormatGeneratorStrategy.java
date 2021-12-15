ab40fda4778c8cce9e44c084a773de328a1385a3
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 02:50:29 -0600

    Create DateTimeFormatGeneratorStrategy.java

---
+++ .github/workflows/DateTimeFormatGeneratorStrategy.java
@@ -0,0 +1,19 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.FormatGeneratorStrategy;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatGeneratorStrategy implements FormatGeneratorStrategy {
+
+    @Override
+    public String getAlmostValidValue() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:'Z'").format(LocalDateTime.now());
+    }
+
+    @Override
+    public String getTotallyWrongValue() {
+        return "1000-07-21T88:32:28Z";
+    }
+}
