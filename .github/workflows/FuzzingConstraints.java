1a3e9140144aefb53dc40150c7d4588808b4ec66
Phantom <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-12-15 03:25:21 -0600

    Create FuzzingConstraints.java

---
+++ .github/workflows/FuzzingConstraints.java
@@ -0,0 +1,20 @@
+package com.endava.cats.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class FuzzingConstraints {
+    private final boolean hasMinlength;
+    private final boolean hasRequiredFieldsFuzzed;
+
+
+    public boolean hasMinLengthOrMandatoryFieldsFuzzed() {
+        return hasMinlength || hasRequiredFieldsFuzzed;
+    }
+
+    public String getRequiredString() {
+        return (String.valueOf(hasRequiredFieldsFuzzed).toUpperCase()) + (hasMinlength && !hasRequiredFieldsFuzzed ? " but has minLength" : "");
+    }
+}
